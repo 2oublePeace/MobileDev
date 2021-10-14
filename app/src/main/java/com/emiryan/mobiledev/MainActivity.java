73a797c2ae90a7ffa127218532b586fef318e38f
2oublePeace <vova.emiryan@yandex.ru>
2021-10-14 16:12:39 +0400

    done

--- app/src/main/java/com/emiryan/mobiledev/MainActivity.java
+++ app/src/main/java/com/emiryan/mobiledev/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.nio.charset.StandardCharsets;
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<String> list;
     ArrayAdapter<String> adapter;
-    CustomListAdapter customListAdapter;
+    ArrayAdapter<String> customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +34,24 @@ public class MainActivity extends AppCompatActivity {
         editTextName = findViewById(R.id.editTextName);
 
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, list);
-        customListAdapter = new CustomListAdapter(this, list);
+        customAdapter = new CustomAdapter(this, R.layout.item_list, list);
 
         listView = findViewById(R.id.listView);
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        //listView.setAdapter(adapter);
-        listView.setAdapter(customListAdapter);
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                customAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     public void addText(View view) {
-        adapter.add(editTextName.getText().toString());
+        listView.setItemChecked(list.size(), true);
+        customAdapter.add(editTextName.getText().toString());
     }
 
     public void selectAll(View view) {
@@ -58,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     public void printSelectedItems(View view)  {
 
         SparseBooleanArray parsedList = listView.getCheckedItemPositions();
-        StringBuilder checkedItemsList= new StringBuilder();
+        StringBuilder checkedItemsList = new StringBuilder();
 
         for(int i = 0; i < parsedList.size(); i++) {
             if(parsedList.valueAt(i) == true){
