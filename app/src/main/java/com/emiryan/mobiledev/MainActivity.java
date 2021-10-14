5b887dac620b5dc42a3dfd2591bd93b4a8a01038
2oublePeace <vova.emiryan@yandex.ru>
2021-10-14 04:09:05 +0400

    вщут

--- app/src/main/java/com/emiryan/mobiledev/MainActivity.java
+++ app/src/main/java/com/emiryan/mobiledev/MainActivity.java
@@ -3,11 +3,14 @@ package com.emiryan.mobiledev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,20 +19,53 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<String> list;
     ArrayAdapter<String> adapter;
+    CustomListAdapter customListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        editTextName = findViewById(R.id.editTextName);
         list = new ArrayList<>();
+
+        editTextName = findViewById(R.id.editTextName);
+
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, list);
-        listView.setAdapter(adapter);
+        customListAdapter = new CustomListAdapter(this, list);
+
+        listView = findViewById(R.id.listView);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        //listView.setAdapter(adapter);
+        listView.setAdapter(customListAdapter);
     }
 
-    public void editTextChanged(View view) {
+    public void addText(View view) {
         adapter.add(editTextName.getText().toString());
+    }
+
+    public void selectAll(View view) {
+        for (int i = 0; i < list.size(); i++) {
+            listView.setItemChecked(i, true);
+        }
+    }
+
+    public void resetAll(View view) {
+        for (int i = 0; i < list.size(); i++) {
+            listView.setItemChecked(i, false);
+        }
+    }
+
+    public void printSelectedItems(View view)  {
+
+        SparseBooleanArray parsedList = listView.getCheckedItemPositions();
+        StringBuilder checkedItemsList= new StringBuilder();
+
+        for(int i = 0; i < parsedList.size(); i++) {
+            if(parsedList.valueAt(i) == true){
+                String text = (String) listView.getItemAtPosition(i);
+                checkedItemsList = checkedItemsList.append(" " + text);
+            }
+        }
+        Toast.makeText(this, "Selected items are: " + checkedItemsList.toString(), Toast.LENGTH_LONG).show();
     }
 }
